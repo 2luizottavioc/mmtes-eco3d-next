e925f09533a4f6f758a4f8e4a06de21aba22ee0e
marcellydias <fdmarcelly@gmail.com>
2024-05-18 15:21:42 -0300

    Changes not staged for commit: 	modified:   src/app/components/Sidebar.js

---
+++ src/app/components/Sidebar.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import eco3dLogo from "../public/eco3dLogo.png"; // ajuste o caminho conforme necessário
+
+const Sidebar = () => {
+  return (
+    <aside className="bg-black w-64 flex justify-center items-end h-screen">
+      <Image src={eco3dLogo} width={100} height={100} alt="Eco 3D Logo" />
+    </aside>
+  );
+};
+
+export default Sidebar;
