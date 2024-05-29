23db79b97bce734fb9f6d998041ba7203dba8a37
marcellydias <fdmarcelly@gmail.com>
2024-05-29 20:38:19 -0300

    Changes to be committed: 	modified:   public/mulher-reciclagem.png 	modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div>
       <header>
         <nav className="bg-black">
-          <div className="container mx-auto px-4 flex justify-between items-center py-3">
+          <div className="container mx-4 px-20 flex justify-between items-center py-3">
             <a className="navbar-brand" href="#">
               <Image
                 src="/eco3d-logo.png"
@@ -15,9 +15,9 @@ export default function Home() {
                 alt="Eco3D Logo"
               />
             </a>
-            <button className="text-white focus:outline-none md:hidden">
+            <button className="text-white focus:outline-none md:hidden ">
               <svg
-                className="w-6 h-6"
+                className="w-6 h-6 "
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -31,19 +31,23 @@ export default function Home() {
                 ></path>
               </svg>
             </button>
-            <div className="hidden md:flex space-x-4">
-              <a href="#home" className="text-white">
+
+            <div className="hidden md:flex space-x-4 ">
+              <a href="#home" className="text-white hover:text-primary-700">
                 Home
               </a>
-              <a href="#recursos" className="text-white">
+              <a href="#recursos" className="text-white hover:text-primary-700">
                 Recursos
               </a>
-              <a href="#beneficios" className="text-white">
+              <a
+                href="#beneficios"
+                className="text-white hover:text-primary-700"
+              >
                 Benefícios
               </a>
               <a
                 href="#"
-                className="ml-5 px-4 py-2 border border-white text-white rounded-lg"
+                className="ml-4 px-3 py-1 border border-white text-white rounded-md  hover:bg-white hover:text-black"
               >
                 Entrar
               </a>
@@ -52,19 +56,21 @@ export default function Home() {
         </nav>
       </header>
 
-      <section id="home" className="bg-green-900 text-white py-20">
+      <section id="home" className="bg-green-900 text-white py-15 px-20">
         <div className="container mx-auto px-4 flex flex-col md:flex-row">
           <div className="md:w-2/3 flex items-center">
             <div>
               <h1 className="text-4xl md:text-5xl font-bold">Eco 3D</h1>
-              <p className="mt-4">
-                Gerenciamento de estoque usado para controle de materiais
-                recicláveis para filamentos de impressora 3D!
+              <p className="mt-4 text-3xl">
+                Gerenciamento de{" "}
+                <span className="text-primary-400">estoque</span> usado para
+                controle de materiais recicláveis para filamentos de{" "}
+                <span className="text-primary-400">impressora 3D!</span>
               </p>
               <form className="mt-4 mb-4">
                 <button
                   type="button"
-                  className="bg-black text-white py-2 px-4 rounded-lg"
+                  className="bg-black text-primary-700 font-bold py-2 px-4 rounded-lg hover:text-primary-500 "
                 >
                   Cadastre-se
                 </button>
@@ -83,10 +89,10 @@ export default function Home() {
         </div>
       </section>
 
-      <section id="recursos" className="py-20 border-b">
+      <section id="recursos" className="py-20  border-b">
         <div className="container mx-auto px-4">
           <div className="flex flex-col md:flex-row">
-            <div className="md:w-1/2 flex items-center">
+            <div className="md:w-1/2 flex items-center px-20">
               <div>
                 <h2 className="text-3xl font-bold">Gestão de produtos</h2>
                 <p className="mt-4">
@@ -95,7 +101,7 @@ export default function Home() {
                 </p>
               </div>
             </div>
-            <div className="md:w-1/2">
+            <div className="md:w-1/2 ">
               <Image
                 src="/tela-produtos.png"
                 alt="Tela de Produtos"
@@ -110,8 +116,8 @@ export default function Home() {
 
       <section className="py-20 border-b">
         <div className="container mx-auto px-4">
-          <div className="flex flex-col md:flex-row">
-            <div className="md:w-1/2">
+          <div className="flex flex-col md:flex-row px-20">
+            <div className="md:w-1/2 ">
               <Image
                 src="/tela-vendas.png"
                 alt="Tela de Vendas"
@@ -135,19 +141,19 @@ export default function Home() {
         </div>
       </section>
 
-      <section id="beneficios" className="py-20 border-b">
+      <section id="beneficios" className="pb-20 pt-15 border-b">
         <div className="container mx-auto px-4">
-          <div className="flex flex-col md:flex-row">
-            <div className="md:w-1/3 text-center mb-8 md:mb-0">
+          <div className="flex flex-col md:flex-row px-20">
+            <div className="md:w-1/3 text-center mb-4 md:mb-0">
               <Image
                 src="/interface.png"
                 alt="Interface"
                 width={500}
                 height={500}
-                className="mx-auto mb-4"
+                className="mx-auto"
               />
               <h2 className="text-2xl font-bold">Fácil de usar</h2>
-              <p className="mt-4">
+              <p className="mt-2">
                 Ferramentas intuitivas e usabilidade acessível.
               </p>
             </div>
@@ -157,10 +163,10 @@ export default function Home() {
                 alt="Reciclagem"
                 width={500}
                 height={500}
-                className="mx-auto mb-4"
+                className="mx-auto"
               />
               <h2 className="text-2xl font-bold">Propósito sustentável</h2>
-              <p className="mt-4">
+              <p className="mt-2">
                 Projeto sustentável e favorável ao meio ambiente.
               </p>
             </div>
@@ -170,35 +176,30 @@ export default function Home() {
                 alt="Suporte"
                 width={500}
                 height={500}
-                className="mx-auto mb-4"
+                className="mx-auto"
               />
               <h2 className="text-2xl font-bold">Suporte amigo</h2>
-              <p className="mt-4">Conte com o nosso suporte a qualquer hora!</p>
+              <p className="mt-2">Conte com o nosso suporte a qualquer hora!</p>
             </div>
           </div>
         </div>
       </section>
 
-      <footer className="py-8 bg-gray-800 text-white">
+      <footer className="py-8 bg-black text-white">
         <div className="container mx-auto px-4">
-          <div className="flex flex-col md:flex-row justify-between">
+          <div className="flex flex-col md:flex-row justify-between px-20">
             <div>
               <p className="mb-4">
-                <a href="#home" className="mr-4">
+                <a href="#home" className="mr-4 hover:text-primary-700">
                   Home
                 </a>
-                <a href="#recursos" className="mr-4">
+                <a href="#recursos" className="mr-4 hover:text-primary-700">
                   Recursos
                 </a>
-                <a href="#beneficios" className="mr-4">
+                <a href="#beneficios" className="mr-4 hover:text-primary-700">
                   Benefícios
                 </a>
               </p>
-            </div>
-            <div className="flex items-center space-x-4">
-              <a href="#" className="text-white">
-                <i className="fab fa-github"></i>
-              </a>
             </div>
           </div>
         </div>
